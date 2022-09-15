2744afbdf63360a2b6e48fe1f2052217fb9f6b1f
Nelio <nelio.lucas@dicemz.com>
2022-09-15 10:10:38 +0200

    fix stacked list section 2

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,7 @@ import 'package:digidexplus/utils/digimon_utils.dart';
 import 'package:digidexplus/utils/retro-client.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:palette_generator/palette_generator.dart';
 
 import 'components/stacked_list.dart';
@@ -23,6 +24,9 @@ class _HomeState extends State<Home> {
   int page = 0;
   List<DigimonCard>? digimon = [];
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final ScrollController _controller = ScrollController();
+
+  bool loading = false;
 
   @override
   void initState() {
@@ -32,6 +36,7 @@ class _HomeState extends State<Home> {
     dio.options.headers["Demo-Header"] = "demo header"; // config your dio headers globally
     client = RestClient(dio);
     _loadItems();
+    _initlisteners();
   }
 
   @override
@@ -39,41 +44,70 @@ class _HomeState extends State<Home> {
     if (mounted) super.setState(fn);
   }
 
-  _loadItems() {
-    client.getPaginatedDigimon(page).then((values) {
-      values.content?.forEach((element) {
-        print("content $element");
-        Digimon item = Digimon.$DigimonFromJson(element);
-        client.getDigimon("${item.id}").then((details) => _getPaletteGenerator(details));
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // use block pattern
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: Text(
-          "DigiDex",
-          style: TextStyle(color: Colors.grey[800], fontSize: 20),
-        ),
-        centerTitle: true,
-      ),
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: Colors.transparent,
+      //   title: Column(
+      //     mainAxisSize: MainAxisSize.min,
+      //     children: [
+      //       Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: Text(
+      //           "DigiDex",
+      //           style: TextStyle(color: Colors.grey[800], fontSize: 20),
+      //         ),
+      //       ),
+      //       Visibility(
+      //           child: SpinKitFadingCube(
+      //         color: Colors.grey[800],
+      //         size: 20.0,
+      //       ))
+      //     ],
+      //   ),
+      //   centerTitle: true,
+      // ),
       body: SafeArea(
         child: Center(
-          child: StackedListView(
-            key: _scaffoldKey,
-            padding: EdgeInsets.only(left: 20, right: 20, top: 50),
-            itemCount: digimon!.length,
-            itemExtent: 400,
-            heightFactor: 0.7,
-            fadeOutFrom: 0.7,
-            builder: (_, index) {
-              return digimon![index];
-            },
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0, bottom: 16),
+                      child: Text(
+                        "DigiDex",
+                        style: TextStyle(color: Colors.grey[800], fontSize: 20),
+                      ),
+                    ),
+                    Visibility(
+                        visible: loading,
+                        child: SpinKitFadingCube(
+                          color: Colors.grey[800],
+                          size: 20.0,
+                        ))
+                  ],
+                ),
+                StackedListView(
+                  controller: _controller,
+                  key: _scaffoldKey,
+                  padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  itemCount: digimon!.length,
+                  itemExtent: 400,
+                  heightFactor: 0.7,
+                  fadeOutFrom: 0.7,
+                  builder: (_, index) {
+                    return digimon![index];
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -97,7 +131,7 @@ class _HomeState extends State<Home> {
         imageLink: image,
       ));
     });
-
+    loading = false;
     digimon = digimon?.toSet().toList();
   }
 
@@ -105,15 +139,33 @@ class _HomeState extends State<Home> {
     return paletteMap[href];
   }
 
-  _updateList(int index, size) {
-    print(index);
-    print(size);
-    bool endOfList = index >= size - 1;
+  _updateList(endOfList) {
     if (endOfList) {
       page++;
       setState(() {
         print("Reloading List");
+        _loadItems();
       });
     }
+  }
+
+  _initlisteners() {
+    _controller.addListener(() {
+      bool end = _controller.position.pixels > 0 && _controller.position.atEdge;
+      _updateList(end);
+    });
+  }
+
+  _loadItems() {
+    setState(() {
+      loading = true;
+    });
+    client.getPaginatedDigimon(page).then((values) {
+      values.content?.forEach((element) {
+        print("content $element");
+        Digimon item = Digimon.$DigimonFromJson(element);
+        client.getDigimon("${item.id}").then((details) => _getPaletteGenerator(details));
+      });
+    });
   }
 }
